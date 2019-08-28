8e8b33a588bc668e90781ba78b7a7a96baa8e80a
Elizabeth Ter Sahakyan <liztersahakyan@gmail.com>
2019-08-28 08:55:28 -0700

    update model

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import pandas as pd
 import numpy as np
+import xgboost as xgb
 from flask import Flask, jsonify, request
 import pickle
 
 # model
-# my_model = pickle.load(open('/tuesday_model_6.pkl','rb'))
-my_model = pickle.load(open('tuesday_model_6.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
 app = Flask(__name__)
 
@@ -14,28 +15,28 @@ def make_predict():
     #get data
     data = request.get_json(force=True)
 
-    # testing on tuesday model 6
     predict_request = [data['neighborhood'],
                        data['room_type'],
                        data['accommodates'],
                        data['bedrooms'],
-                       data['bathrooms'],
                        data['number_of_reviews'],
                        data['wifi'],
                        data['cable_tv'],
                        data['washer'],
                        data['kitchen']]
 
+    data.update((x, [y]) for x, y in data.items())
+    data_df = pd.DataFrame.from_dict(data)
 
-    predict_request = np.array(predict_request).reshape(1,-1)
-
-    #preds
-    y_hat = my_model.predict(predict_request)
+    # preds
+    y_hat = model.predict(data_df) # this works for xgb
 
     # send back to browser
     output = {'y_hat': int(y_hat[0])}
+
+    # return data
     return jsonify(results=output)
 
-
 if __name__ == '__main__':
-    app.run(port = 5000, debug=True)
+    app.run(port = 9000, debug=True)
+
